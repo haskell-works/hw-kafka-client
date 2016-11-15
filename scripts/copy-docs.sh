904f1815cf49f4b11aec0845a306da0de9990777
Alexey Raga <alexey.raga@gmail.com>
2016-11-16 08:52:42 +1100

    modernize ci

---
+++ scripts/copy-docs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+cabal_file=$(find . -maxdepth 1 -name "*.cabal" -print -quit)
+if [ ! -f "$cabal_file" ]; then
+  echo "Run this script in the top-level package directory"
+  exit 1
+fi
+
+pkg=$(awk -F ":[[:space:]]*" 'tolower($1)=="name"    { print $2 }' < "$cabal_file")
+ver=$(awk -F ":[[:space:]]*" 'tolower($1)=="version" { print $2 }' < "$cabal_file")
+
+if [ -z "$pkg" ]; then
+  echo "Unable to determine package name"
+  exit 1
+fi
+
+if [ -z "$ver" ]; then
+  echo "Unable to determine package version"
+  exit 1
+fi
+
+echo "Detected package: $pkg-$ver"
+
+cp -R $(stack path --local-install-root)/doc/$pkg-$ver/ $CIRCLE_ARTIFACTS/$pkg-$ver-docs
