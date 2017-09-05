6e97d53deb5afd88fa7aa7e8446577beb10981d5
Alexey Raga <alexey.raga@gmail.com>
2017-09-06 01:28:06 +1000

    Release 2.0

--- scripts/linux-test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set +e
-_image=fpco/stack-build:latest
-docker run --rm -v $PWD:/bld -it $_image bash -c "cd /bld && ./scripts/build-librdkafka.sh"
-echo "Building the project"
-stack --docker --docker-image=$_image test \
-        --extra-include-dirs "$PWD/.librdkafka/include/librdkafka" \
-        --extra-lib-dirs "$PWD/.librdkafka/lib"
