331ee32534ea7887d22f31739d56be7df942bf3c
Alexey Raga <alexey.raga@gmail.com>
2017-11-04 15:04:04 +1100

    Remove "include-dirs" and "extra-lib-dirs"

--- build/integration-tests.sh
+++ build/integration-tests.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 export DOCKER_IP=`ifconfig | grep "inet addr:" | grep "Bcast:0.0.0.0" | cut -d: -f2 | awk '{ print $1}'`
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 
 stack clean
 KAFKA_TEST_BROKER=$DOCKER_IP stack test
