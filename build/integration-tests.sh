5a7d0f86446bf0beb09a1147a7689d810eed4207
chessai <chessai1996@gmail.com>
2018-09-05 10:33:35 -0400

    rewrite integration-tests.sh

--- build/integration-tests.sh
+++ build/integration-tests.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-export DOCKER_IP=`ifconfig | grep "inet addr:" | grep "Bcast:0.0.0.0" | cut -d: -f2 | awk '{ print $1}'`
-
-KAFKA_TEST_BROKER=$DOCKER_IP
-
-nix-build
+nix-shell --run " export DOCKER_IP=`ifconfig | grep "inet addr:" | grep "Bcast:0.0.0.0" | cut -d: -f2 | awk '{ print $1}'`; \
+                  KAFKA_TEST_BROKER=$DOCKER_IP; \
+                  docker-compose up -d; \
+                  sleep 2; \
+                  nix-build; \
+                "
