e822010cfd7012a0d597e41982b49e67bb47672d
Levashov, Mykyta <mykyta.levashov@xa.epicgames.com>
2021-09-09 14:06:33 +0300

    Fix tests

--- shell.nix
+++ shell.nix
@@ -7,6 +7,7 @@ pkgs.mkShell {
     rdkafka
     nettools
     niv
+    gmp
   ];
 
   shellHook = ''
--- tests/Kafka/Consumer/ConsumerRecordTraverseSpec.hs
+++ tests/Kafka/Consumer/ConsumerRecordTraverseSpec.hs
@@ -21,6 +21,7 @@ testRecord = ConsumerRecord
   , crPartition = PartitionId 0
   , crOffset    = Offset 5
   , crTimestamp = NoTimestamp
+  , crHeaders   = mempty
   , crKey       = testKey
   , crValue     = testValue
   }
