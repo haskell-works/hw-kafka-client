87dc3c5064596f7ce5395694095edf04c571e745
Jonathan <jonathan@genlambda.com>
2022-09-16 09:10:06 +0200

    added TxError to other transaction functions.

--- src/Kafka/Transaction.hs
+++ src/Kafka/Transaction.hs
@@ -35,45 +35,37 @@ data TxError = TxError !KafkaError !InternalError
 
 -- | Initialises Kafka for transactions 
 initTransactions :: MonadIO m => KafkaProducer -> Timeout -> m (Maybe TxError)
-initTransactions p (Timeout to) = liftIO $ do
-  errPtr <- rdKafkaInitTransactions (getRdKafka p) to
-  ret  <- rdKafkaErrorCode errPtr >>= handleProduceErrT
-  case ret of
-    Nothing -> pure Nothing
-    Just ke -> pure $ Just $ TxError ke errPtr 
+initTransactions p (Timeout to) = liftIO $ rdKafkaInitTransactions (getRdKafka p) to >>= toTxError
 
 -- | Begins a new transaction
-beginTransaction :: MonadIO m => KafkaProducer -> m (Maybe KafkaError)
-beginTransaction p = liftIO $ do
-  rdKafkaBeginTransaction (getRdKafka p) >>= rdKafkaErrorCode >>= handleProduceErrT
+beginTransaction :: MonadIO m => KafkaProducer -> m (Maybe TxError)
+beginTransaction p = liftIO $ rdKafkaBeginTransaction (getRdKafka p) >>= toTxError
 
 -- | Commits an existing transaction
 -- Pre-condition: there exists an open transaction, created with beginTransaction
-commitTransaction :: MonadIO m => KafkaProducer -> Timeout -> m (Maybe KafkaError)
-commitTransaction p (Timeout to) = liftIO $ do
-  rdKafkaCommitTransaction (getRdKafka p) to >>= rdKafkaErrorCode >>= handleProduceErrT
+commitTransaction :: MonadIO m => KafkaProducer -> Timeout -> m (Maybe TxError)
+commitTransaction p (Timeout to) = liftIO $ rdKafkaCommitTransaction (getRdKafka p) to >>= toTxError
 
 -- | Aborts an existing transaction
 -- Pre-condition: there exists an open transaction, created with beginTransaction
-abortTransaction :: MonadIO m => KafkaProducer -> Timeout -> m (Maybe KafkaError)
-abortTransaction p (Timeout to) = liftIO $ do
-  rdKafkaAbortTransaction (getRdKafka p) to >>= rdKafkaErrorCode >>= handleProduceErrT
+abortTransaction :: MonadIO m => KafkaProducer -> Timeout -> m (Maybe TxError)
+abortTransaction p (Timeout to) = liftIO $ do rdKafkaAbortTransaction (getRdKafka p) to >>= toTxError
 
 -- | Commits the message's offset in the current transaction
 --    Similar to Kafka.Consumer.commitOffsetMessage but within a transactional context
 -- Pre-condition: there exists an open transaction, created with beginTransaction
-commitOffsetMessageTransaction :: MonadIO m => KafkaProducer -> KafkaConsumer -> ConsumerRecord k v -> Timeout -> m (Maybe KafkaError)
+commitOffsetMessageTransaction :: MonadIO m => KafkaProducer -> KafkaConsumer -> ConsumerRecord k v -> Timeout -> m (Maybe TxError)
 commitOffsetMessageTransaction p c m (Timeout to) = liftIO $ do
   tps <- toNativeTopicPartitionList [topicPartitionFromMessageForCommit m]
-  rdKafkaSendOffsetsToTransaction (getRdKafka p) (getRdKafka c) tps to >>= rdKafkaErrorCode >>= handleProduceErrT
+  rdKafkaSendOffsetsToTransaction (getRdKafka p) (getRdKafka c) tps to >>= toTxError
 
 -- | Commit offsets for all currently assigned partitions in the current transaction
 --    Similar to Kafka.Consumer.commitAllOffsets but within a transactional context
 -- Pre-condition: there exists an open transaction, created with beginTransaction
-commitAllOffsetsTransaction :: MonadIO m => KafkaProducer -> KafkaConsumer -> Timeout -> m (Maybe KafkaError)
+commitAllOffsetsTransaction :: MonadIO m => KafkaProducer -> KafkaConsumer -> Timeout -> m (Maybe TxError)
 commitAllOffsetsTransaction p c (Timeout to) = liftIO $ do
   tps <- newForeignPtr_ nullPtr
-  rdKafkaSendOffsetsToTransaction (getRdKafka p) (getRdKafka c) tps to >>= rdKafkaErrorCode >>= handleProduceErrT
+  rdKafkaSendOffsetsToTransaction (getRdKafka p) (getRdKafka c) tps to >>= toTxError
 
 getKafkaError :: TxError -> KafkaError
 getKafkaError (TxError kErr _) = kErr
@@ -87,5 +79,11 @@ kafkaErrorIsRetriable (TxError _ internalErr) = unsafePerformIO $ rdKafkaErrorIs
 kafkaErrorTxnRequiresAbort :: TxError -> Bool
 kafkaErrorTxnRequiresAbort (TxError _ internalErr) = unsafePerformIO $ rdKafkaErrorTxnRequiresAbort internalErr
 
-
-
+----------------------------------------------------------------------------------------------------
+-- Implementation detail, used internarlly
+toTxError :: RdKafkaErrorTPtr -> IO (Maybe TxError)
+toTxError errPtr = do
+  ret  <- rdKafkaErrorCode errPtr >>= handleProduceErrT
+  case ret of
+    Nothing -> pure Nothing
+    Just ke -> pure $ Just $ TxError ke errPtr 
