eb193f5cacf7a8bf29bd6da81085084b14c72bb3
JoranVanBelle <joran.vanbelle@live.be>
2025-05-21 08:53:22 +0200

    fix(admin): make creation unsafe and wait for response

--- src/Kafka/Topic.hs
+++ src/Kafka/Topic.hs
@@ -4,14 +4,21 @@ module X
 , deleteTopic
 ) where
 
+import           Control.Exception
 import           Control.Monad.IO.Class
+import           Control.Monad.Trans.Class
+import           Control.Monad.Trans.Except
+import           Control.Monad.Trans.Maybe
+import           Data.Bifunctor
+import           Data.Foldable
 import           Data.List.NonEmpty
-import qualified Data.List.NonEmpty     as NEL
-import           Data.Text
-import qualified Data.Text              as T
-
-import Kafka.Internal.RdKafka
-import Kafka.Internal.Setup
+import qualified Data.List.NonEmpty         as NEL
+import qualified Data.Map                   as M
+import           Data.Maybe
+import qualified Data.Set                   as S
+import qualified Data.Text                  as T
+import           Kafka.Internal.RdKafka
+import           Kafka.Internal.Setup
 
 import Kafka.Topic.Types as X
 import Kafka.Types       as X
@@ -24,11 +31,17 @@ createTopic k topic = do
   opts <- newRdKAdminOptions kafkaPtr RdKafkaAdminOpAny
 
   topicRes <- withNewTopic topic $ \topic' -> rdKafkaCreateTopic kafkaPtr topic' opts queue
+
   case topicRes of
     Left err -> do
       pure $ Left (NEL.head err)
     Right _ -> do
-      pure $ Right $ topicName topic
+      res <- waitForResponse (topicName topic) rdKafkaEventCreateTopicsResult rdKafkaCreateTopicsResultTopics queue
+      case listToMaybe res of
+        Nothing -> pure $ Left KafkaInvalidReturnValue
+        Just result -> pure $ case result of
+          Left (_, e, _) -> Left e
+          Right tName    -> Right tName
 
 --- DELETE TOPIC ---
 deleteTopic :: HasKafka k
@@ -45,19 +58,17 @@ deleteTopic k topic = liftIO $ do
     Left err -> do
       pure $ Left (NEL.head err)
     Right _ -> do
-      pure $ Right topic
+      res <- waitForResponse topic rdKafkaEventDeleteTopicsResult rdKafkaDeleteTopicsResultTopics queue
+      case listToMaybe res of
+        Nothing -> pure $ Left KafkaInvalidReturnValue
+        Just result -> pure $ case result of
+          Left (_, e, _) -> Left e
+          Right tName    -> Right tName
 
 withNewTopic :: NewTopic
                 -> (RdKafkaNewTopicTPtr ->  IO a)
                 -> IO (Either (NonEmpty KafkaError) a)
-withNewTopic t transform = do
-  mkNewTopicRes <- mkNewTopic t newTopicPtr
-  case mkNewTopicRes of
-    Left err -> do
-      return $ Left err
-    Right topic -> do
-      res <- transform topic
-      return $ Right res
+withNewTopic t = withUnsafeOne t mkNewTopicUnsafe rdKafkaNewTopicDestroy
 
 withOldTopic :: TopicName
                 -> (RdKafkaDeleteTopicTPtr -> IO a)
@@ -71,28 +82,21 @@ withOldTopic tName transform = do
       res <- transform topic
       return $ Right res
 
-newTopicPtr :: NewTopic -> IO (Either KafkaError RdKafkaNewTopicTPtr)
-newTopicPtr topic = do
-  ptrRes <- newRdKafkaNewTopic (unpack $ unTopicName $ topicName topic) (unPartitionCount $ topicPartitionCount topic) (unReplicationFactor $ topicReplicationFactor topic)
-  case ptrRes of
-    Left str  -> pure $ Left (KafkaError $ T.pack str)
-    Right ptr -> pure $ Right ptr
-
 oldTopicPtr :: TopicName -> IO (Either KafkaError RdKafkaDeleteTopicTPtr)
 oldTopicPtr tName = do
-  res <- newRdKafkaDeleteTopic $ unpack . unTopicName $ tName
+  res <- newRdKafkaDeleteTopic $ T.unpack . unTopicName $ tName
   case res of
     Left str  -> pure $ Left (KafkaError $ T.pack str)
     Right ptr -> pure $ Right ptr
 
-mkNewTopic :: NewTopic
-              -> (NewTopic -> IO (Either KafkaError a))
-              -> IO (Either (NonEmpty KafkaError) a)
-mkNewTopic topic create = do
-  res <- create topic
-  case res of
-    Left err       -> pure $ Left (singletonList err)
-    Right resource -> pure $ Right resource
+mkNewTopicUnsafe :: NewTopic -> IO (Either KafkaError RdKafkaNewTopicTPtr)
+mkNewTopicUnsafe topic = runExceptT $ do
+  topic' <- withErrStr $ newRdKafkaNewTopicUnsafe (T.unpack $ unTopicName $ topicName topic) (unPartitionCount $ topicPartitionCount topic) (unReplicationFactor $ topicReplicationFactor topic)
+  _ <- withErrKafka $ whileRight (uncurry $ rdKafkaNewTopicSetConfig undefined) (M.toList $ topicConfig topic)
+  pure topic'
+    where
+      withErrStr = withExceptT (KafkaError . T.pack) . ExceptT
+      withErrKafka = withExceptT KafkaResponseError . ExceptT
 
 rmOldTopic :: TopicName
             -> (TopicName -> IO (Either KafkaError a))
@@ -103,5 +107,52 @@ rmOldTopic tName remove = do
     Left err       -> pure $ Left (singletonList err)
     Right resource -> pure $ Right resource
 
+withUnsafeOne :: a                               -- ^ Item to handle
+              -> (a -> IO (Either KafkaError b)) -- ^ Create an unsafe element
+              -> (b -> IO ())                    -- ^ Destroy the unsafe element
+              -> (b -> IO c)                     -- ^ Handler
+              -> IO (Either (NonEmpty KafkaError) c)
+withUnsafeOne a mkOne cleanup f =
+  bracket (mkOne a) cleanupOne processOne
+  where
+    cleanupOne (Right b) = cleanup b
+    cleanupOne (Left _)  = pure () -- no resource to clean if creation failed
+
+    processOne (Right b) = Right <$> f b
+    processOne (Left e)  = pure (Left (singletonList e))
+
+whileRight :: Monad m
+           => (a -> m (Either e ()))
+           -> [a]
+           -> m (Either e ())
+whileRight f as = runExceptT $ traverse_ (ExceptT . f) as
+
+waitForResponse :: TopicName
+                -> (RdKafkaEventTPtr -> IO (Maybe a))
+                -> (a -> IO [Either (String, RdKafkaRespErrT, String) String])
+                -> RdKafkaQueueTPtr
+                -> IO [Either (TopicName, KafkaError, String) TopicName]
+waitForResponse topic fromEvent toResults q =
+  fromMaybe [] <$> runMaybeT (go [])
+  where
+    awaited = S.singleton topic
+
+    go accRes = do
+      qRes <- MaybeT $ rdKafkaQueuePoll q 1000
+      eRes <- MaybeT $ fromEvent qRes
+      tRes <- lift $ toResults eRes
+      let results = wrapTopicName <$> tRes
+      let topics  = S.fromList $ getTopicName <$> results
+      let newRes  = results <> accRes
+      let remaining = S.difference awaited topics
+      if S.null remaining
+        then pure newRes
+        else go newRes
+
+    getTopicName = either (\(t,_,_) -> t) id
+    wrapTopicName = bimap (\(t,e,s) -> (TopicName (T.pack t), KafkaResponseError e, s))
+                          (TopicName . T.pack)
+
 singletonList :: a -> NonEmpty a
 singletonList x = x :| []
+
